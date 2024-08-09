c355267629938889122713703b5c5c16cc0191e9
hikmat-ullah <hikmatmarwat644@gmail.com>
2024-08-09 15:51:02 +0500

    Done fixes in build and test folders

--- jenkins/build/mvntest.sh
+++ jenkins/build/mvntest.sh
@@ -8,6 +8,8 @@ echo "************************"
 # The version of docker image of maven used in the course is not working
 # so i changed the maven image version for building & testing
 
-docker run --rm -v $PWD/java-app:/app -v /root/.m2:/root/.m2 -w /app maven:3.6.3-jdk-8-slim "$@"
+WORKSPACE=/home/admin/jenkins-data/jenkins_home/workspace/maven-pipeline
+
+docker run --rm -v $WORKSPACE/java-app:/app -v /root/.m2:/root/.m2 -w /app maven:3.6.3-jdk-8-slim "$@"
 
 
--- jenkins/test/mvntest.sh
+++ jenkins/test/mvntest.sh
@@ -4,4 +4,6 @@ echo "***************************"
 echo "*** Testing the code ******"
 echo "***************************"
 
-docker run --rm  -v $PWD/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3.6.3-jdk-8-slim "$@"
+WORKSPACE=/home/admin/jenkins-data/jenkins_home/workspace/maven-pipeline
+
+docker run --rm  -v $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3.6.3-jdk-8-slim "$@"
