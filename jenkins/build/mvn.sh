ff627586c58beef5ec03a20a71ef52ca1d2645eb
hikmat-ullah <hikmatmarwat644@gmail.com>
2024-08-09 16:31:03 +0500

    Fixed path issue in mvn script

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -4,6 +4,6 @@ echo "***************************"
 echo "** Building jar ***********"
 echo "***************************"
 
-WORKSPACE=/home/jenkins/jenkins-data/jenkins_home/workspace/maven-pipeline
+WORKSPACE=/home/admin/jenkins-data/jenkins_home/workspace/maven-pipeline
 
 docker run --rm  -v  $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
