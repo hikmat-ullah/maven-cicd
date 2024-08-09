438fc6659f9fd855dde6045e6260d452f3f924d5
hikmat-ullah <hikmatmarwat644@gmail.com>
2024-08-09 16:38:34 +0500

    fixed version issue in mvn script

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -6,4 +6,4 @@ echo "***************************"
 
 WORKSPACE=/home/admin/jenkins-data/jenkins_home/workspace/maven-pipeline
 
-docker run --rm  -v  $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
+docker run --rm  -v  $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3.6.3-jdk-8-slim "$@"
