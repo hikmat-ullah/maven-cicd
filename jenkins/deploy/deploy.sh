549b0e8be71379b927e7d49f4488e15e2a212a67
hikmat-ullah <hikmatmarwat644@gmail.com>
2024-08-09 17:10:59 +0500

    fixed deploy script

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -4,6 +4,7 @@ echo maven-project > /tmp/.auth
 echo $BUILD_TAG >> /tmp/.auth
 echo $PASS >> /tmp/.auth
 
+
 scp -i /opt/prod /tmp/.auth admin@192.168.232.133:/tmp/.auth
 scp -i /opt/prod ./jenkins/deploy/publish admin@192.168.232.133:/tmp/publish
 ssh -i /opt/prod admin@192.168.232.133 "/tmp/publish"
